49e6eca649c6713fc84ac92dccf791db1abef2c3
Rupert Madden-Abbott <rupert@madden-abbott.com>
2026-01-18 16:07:45 +0000

    Add some more functions and example images.

--- src/main/java/com/alex/drawing/Application.java
+++ src/main/java/com/alex/drawing/Application.java
@@ -2,6 +2,7 @@ package com.alex.drawing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 import static com.alex.drawing.DrawingPanel.*;
 
@@ -11,75 +12,90 @@ public class Application {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Simple Drawing App");
             DrawingPanel drawingPanel = new DrawingPanel();
-            JPanel colorPanel = new JPanel();
-            Color[] colors = {Color.BLACK, Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.MAGENTA};
-            String[] colorNames = {"Black", "Red", "Blue", "Green", "Orange", "Magenta"};
-            JButton[] colorButtons = new JButton[colors.length];
-            for (int i = 0; i < colors.length; i++) {
-                JButton btn = new JButton(colorNames[i]);
-                Color c = colors[i];
-                btn.setForeground(c);
-                btn.setFocusPainted(false);
-                int idx = i;
-                btn.addActionListener(e -> {
-                    drawingPanel.setCurrentColor(c);
-                    for (int j = 0; j < colorButtons.length; j++) {
-                        colorButtons[j].setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-                    }
-                    btn.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
-                });
-                colorButtons[i] = btn;
-                colorPanel.add(btn);
-            }
-            // Set initial color highlight
-            colorButtons[0].setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
-
-            JPanel toolPanel = new JPanel();
-            JButton pencilBtn = new JButton("Pencil");
-            JButton fillBtn = new JButton("Fill");
-            JButton brushBtn = new JButton("Brush");
-            JButton eraserBtn = new JButton("Eraser");
-            eraserBtn.setFocusPainted(false);
-            JButton clearBtn = new JButton("Clear");
-            JButton[] toolButtons = {pencilBtn, fillBtn, brushBtn, eraserBtn};
-            pencilBtn.setFocusPainted(false);
-            fillBtn.setFocusPainted(false);
-            brushBtn.setFocusPainted(false);
-            pencilBtn.addActionListener(e -> {
-                drawingPanel.setCurrentTool(Tool.PENCIL);
-                for (JButton b : toolButtons) b.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-                pencilBtn.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-            });
-            fillBtn.addActionListener(e -> {
-                drawingPanel.setCurrentTool(Tool.FILL);
-                for (JButton b : toolButtons) b.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-                fillBtn.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-            });
-            brushBtn.addActionListener(e -> {
-                drawingPanel.setCurrentTool(Tool.BRUSH);
-                for (JButton b : toolButtons) b.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-                brushBtn.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-            });
-            eraserBtn.addActionListener(e -> {
-                drawingPanel.setCurrentTool(Tool.ERASER);
-                for (JButton b : toolButtons) b.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-                eraserBtn.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-            });
-            clearBtn.addActionListener(e -> drawingPanel.clearCanvas());
-            toolPanel.add(pencilBtn);
-            toolPanel.add(fillBtn);
-            toolPanel.add(brushBtn);
-            toolPanel.add(eraserBtn);
-            // Set initial tool highlight
-            pencilBtn.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
-
+            // --- New Menus for Tools and Colours ---
             JPanel controlsPanel = new JPanel();
             controlsPanel.setLayout(new BoxLayout(controlsPanel, BoxLayout.X_AXIS));
-            controlsPanel.add(toolPanel);
+
+            // Tool menu button
+            JButton toolMenuBtn = new JButton("Tool");
+            JPopupMenu toolMenu = new JPopupMenu();
+            JMenuItem pencilItem = new JMenuItem("Pencil");
+            JMenuItem brushItem = new JMenuItem("Brush");
+            JMenuItem fillItem = new JMenuItem("Fill");
+            JMenuItem eraserItem = new JMenuItem("Eraser");
+            JMenuItem sprayCanItem = new JMenuItem("Spray Can");
+            JMenu stampMenu = new JMenu("Stamp");
+            JMenuItem[] stampItems = new JMenuItem[4];
+            String[] stampNames = {"Smiley", "Cat", "Star", "Flower"};
+            DrawingPanel.StampType[] stampTypes = {
+                DrawingPanel.StampType.SMILEY,
+                DrawingPanel.StampType.CAT,
+                DrawingPanel.StampType.STAR,
+                DrawingPanel.StampType.FLOWER
+            };
+            for (int i = 0; i < 4; i++) {
+                JMenuItem item = new JMenuItem(stampNames[i]);
+                int idx = i;
+                item.addActionListener(e -> {
+                    drawingPanel.setCurrentTool(Tool.STAMP);
+                    drawingPanel.setCurrentStampType(stampTypes[idx]);
+                    toolMenuBtn.setText("Tool: Stamp - " + stampNames[idx]);
+                });
+                stampItems[i] = item;
+                stampMenu.add(item);
+            }
+            pencilItem.addActionListener(e -> {
+                drawingPanel.setCurrentTool(Tool.PENCIL);
+                toolMenuBtn.setText("Tool: Pencil");
+            });
+            brushItem.addActionListener(e -> {
+                drawingPanel.setCurrentTool(Tool.BRUSH);
+                toolMenuBtn.setText("Tool: Brush");
+            });
+            fillItem.addActionListener(e -> {
+                drawingPanel.setCurrentTool(Tool.FILL);
+                toolMenuBtn.setText("Tool: Fill");
+            });
+            eraserItem.addActionListener(e -> {
+                drawingPanel.setCurrentTool(Tool.ERASER);
+                toolMenuBtn.setText("Tool: Eraser");
+            });
+            sprayCanItem.addActionListener(e -> {
+                drawingPanel.setCurrentTool(Tool.SPRAYCAN);
+                toolMenuBtn.setText("Tool: Spray Can");
+            });
+            toolMenu.add(pencilItem);
+            toolMenu.add(brushItem);
+            toolMenu.add(sprayCanItem);
+            toolMenu.add(fillItem);
+            toolMenu.add(eraserItem);
+            toolMenu.addSeparator();
+            toolMenu.add(stampMenu);
+            toolMenuBtn.addActionListener(e -> toolMenu.show(toolMenuBtn, 0, toolMenuBtn.getHeight()));
+            controlsPanel.add(toolMenuBtn);
             controlsPanel.add(Box.createHorizontalStrut(10));
-            controlsPanel.add(colorPanel);
+
+            // Colour menu button
+            JButton colorMenuBtn = new JButton("Colour");
+            JPopupMenu colorMenu = new JPopupMenu();
+            Color[] colors = {Color.BLACK, Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.MAGENTA};
+            String[] colorNames = {"Black", "Red", "Blue", "Green", "Orange", "Magenta"};
+            for (int i = 0; i < colors.length; i++) {
+                JMenuItem colorItem = new JMenuItem(colorNames[i]);
+                Color c = colors[i];
+                colorItem.setForeground(c);
+                int finalI = i;
+                colorItem.addActionListener(e -> {
+                    drawingPanel.setCurrentColor(c);
+                    colorMenuBtn.setText("Colour: " + colorNames[finalI]);
+                });
+                colorMenu.add(colorItem);
+            }
+            colorMenuBtn.addActionListener(e -> colorMenu.show(colorMenuBtn, 0, colorMenuBtn.getHeight()));
+            controlsPanel.add(colorMenuBtn);
             controlsPanel.add(Box.createHorizontalStrut(10));
-            // Add size selection slider
+
+            // Size slider and clear button
             JPanel sizePanel = new JPanel();
             JLabel sizeLabel = new JLabel("Size:");
             JSlider sizeSlider = new JSlider(1, 30, 5);
@@ -92,7 +108,34 @@ public class Application {
             sizePanel.add(sizeSlider);
             controlsPanel.add(sizePanel);
             controlsPanel.add(Box.createHorizontalStrut(10));
+            JButton clearBtn = new JButton("Clear");
+            clearBtn.addActionListener(e -> drawingPanel.clearCanvas());
             controlsPanel.add(clearBtn);
+            JButton openBtn = new JButton("Open");
+            JButton saveBtn = new JButton("Save");
+            openBtn.addActionListener(e -> {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("PNG Images", "png"));
+                int result = chooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    drawingPanel.openImage(chooser.getSelectedFile());
+                }
+            });
+            saveBtn.addActionListener(e -> {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("PNG Images", "png"));
+                int result = chooser.showSaveDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File file = chooser.getSelectedFile();
+                    if (!file.getName().toLowerCase().endsWith(".png")) {
+                        file = new File(file.getParentFile(), file.getName() + ".png");
+                    }
+                    drawingPanel.saveImage(file);
+                }
+            });
+            controlsPanel.add(openBtn);
+            controlsPanel.add(saveBtn);
+
             frame.add(controlsPanel, BorderLayout.NORTH);
             frame.add(drawingPanel, BorderLayout.CENTER);
             frame.pack();
--- src/main/java/com/alex/drawing/DrawingPanel.java
+++ src/main/java/com/alex/drawing/DrawingPanel.java
@@ -5,6 +5,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.util.Stack;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class DrawingPanel extends JPanel {
     private Color currentColor = Color.BLACK;
@@ -13,6 +16,13 @@ public class DrawingPanel extends JPanel {
     private int lastX, lastY;
     private boolean drawing = false;
     private int toolSize = 5;
+
+    // Cursor preview state
+    private int cursorX = -1, cursorY = -1;
+    private boolean cursorInPanel = false;
+
+    public enum StampType { SMILEY, CAT, STAR, FLOWER }
+    private StampType currentStamp = StampType.SMILEY;
 
     public DrawingPanel() {
         setBackground(Color.WHITE);
@@ -35,6 +45,10 @@ public class DrawingPanel extends JPanel {
                     drawBrush(lastX, lastY, lastX, lastY, false);
                     drawing = true;
                     repaint();
+                } else if (currentTool == Tool.SPRAYCAN) {
+                    sprayAt(e.getX(), e.getY());
+                    drawing = true;
+                    repaint();
                 } else if (currentTool == Tool.ERASER) {
                     lastX = e.getX();
                     lastY = e.getY();
@@ -43,6 +57,9 @@ public class DrawingPanel extends JPanel {
                     repaint();
                 } else if (currentTool == Tool.FILL) {
                     floodFill(e.getX(), e.getY(), currentColor);
+                    repaint();
+                } else if (currentTool == Tool.STAMP) {
+                    drawStamp(e.getX(), e.getY(), toolSize, currentColor, currentStamp);
                     repaint();
                 }
             }
@@ -70,12 +87,34 @@ public class DrawingPanel extends JPanel {
                     lastX = x;
                     lastY = y;
                     repaint();
+                } else if (drawing && currentTool == Tool.SPRAYCAN) {
+                    sprayAt(x, y);
+                    repaint();
                 } else if (drawing && currentTool == Tool.ERASER) {
                     drawBrush(x, y, lastX, lastY, true);
                     lastX = x;
                     lastY = y;
                     repaint();
                 }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                cursorX = e.getX();
+                cursorY = e.getY();
+                repaint();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                cursorInPanel = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                cursorInPanel = false;
+                repaint();
             }
         };
         addMouseListener(mouseAdapter);
@@ -101,6 +140,8 @@ public class DrawingPanel extends JPanel {
         g2.dispose();
         repaint();
     }
+
+    public void setCurrentStampType(StampType type) { this.currentStamp = type; }
 
     private void floodFill(int x, int y, Color fillColor) {
         int targetColor = canvasImage.getRGB(x, y);
@@ -136,13 +177,238 @@ public class DrawingPanel extends JPanel {
         g2.dispose();
     }
 
+    private void drawStamp(int x, int y, int size, Color color, StampType type) {
+        Graphics2D g2 = canvasImage.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        switch (type) {
+            case SMILEY:
+                drawSmiley(g2, x, y, size, color);
+                break;
+            case CAT:
+                drawCat(g2, x, y, size, color);
+                break;
+            case STAR:
+                drawStar(g2, x, y, size, color);
+                break;
+            case FLOWER:
+                drawFlower(g2, x, y, size, color);
+                break;
+        }
+        g2.dispose();
+    }
+
+    private void drawSmiley(Graphics2D g2, int x, int y, int size, Color color) {
+        // Face
+        g2.setColor(color);
+        g2.fillOval(x - size/2, y - size/2, size, size);
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(Math.max(2, size/20)));
+        g2.drawOval(x - size/2, y - size/2, size, size);
+        // Eyes
+        int eyeW = size/7, eyeH = size/5;
+        int eyeY = y - size/6;
+        g2.setColor(Color.WHITE);
+        g2.fillOval(x - size/4 - eyeW/2, eyeY, eyeW, eyeH);
+        g2.fillOval(x + size/4 - eyeW/2, eyeY, eyeW, eyeH);
+        g2.setColor(Color.BLACK);
+        g2.fillOval(x - size/4 - eyeW/4, eyeY + eyeH/2, eyeW/2, eyeH/2);
+        g2.fillOval(x + size/4 - eyeW/4, eyeY + eyeH/2, eyeW/2, eyeH/2);
+        // Smile (arc)
+        int smileW = size/2, smileH = size/3;
+        int smileX = x - smileW/2, smileY = y + size/8;
+        g2.setStroke(new BasicStroke(Math.max(2, size/18)));
+        g2.drawArc(smileX, smileY, smileW, smileH, 0, -180);
+    }
+
+
+    private void drawCat(Graphics2D g2, int x, int y, int size, Color color) {
+        // Head
+        g2.setColor(color);
+        g2.fillOval(x - size/2, y - size/3, size, size*2/3);
+        // Ears
+        int earH = size/3, earW = size/4;
+        Polygon leftEar = new Polygon();
+        leftEar.addPoint(x - size/3, y - size/3);
+        leftEar.addPoint(x - size/6, y - size/2);
+        leftEar.addPoint(x - size/12, y - size/3);
+        Polygon rightEar = new Polygon();
+        rightEar.addPoint(x + size/3, y - size/3);
+        rightEar.addPoint(x + size/6, y - size/2);
+        rightEar.addPoint(x + size/12, y - size/3);
+        g2.fillPolygon(leftEar);
+        g2.fillPolygon(rightEar);
+        // Face outline
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(Math.max(2, size/18)));
+        g2.drawOval(x - size/2, y - size/3, size, size*2/3);
+        g2.drawPolygon(leftEar);
+        g2.drawPolygon(rightEar);
+        // Eyes
+        g2.setColor(Color.WHITE);
+        g2.fillOval(x - size/6, y - size/10, size/8, size/8);
+        g2.fillOval(x + size/12, y - size/10, size/8, size/8);
+        g2.setColor(Color.BLACK);
+        g2.fillOval(x - size/6 + size/32, y - size/10 + size/32, size/16, size/16);
+        g2.fillOval(x + size/12 + size/32, y - size/10 + size/32, size/16, size/16);
+        // Nose
+        g2.setColor(new Color(255, 192, 203));
+        g2.fillOval(x - size/32, y + size/24, size/16, size/20);
+        g2.setColor(Color.BLACK);
+        g2.drawOval(x - size/32, y + size/24, size/16, size/20);
+        // Mouth
+        g2.drawArc(x - size/24, y + size/12, size/12, size/12, 0, -180);
+        g2.drawArc(x - size/24, y + size/12, size/12, size/12, 0, 180);
+        // Whiskers
+        for (int i = -1; i <= 1; i += 2) {
+            g2.drawLine(x + i*size/16, y + size/16, x + i*size/4, y + size/16 + size/24);
+            g2.drawLine(x + i*size/16, y + size/16, x + i*size/4, y + size/24);
+        }
+    }
+    private void drawStar(Graphics2D g2, int x, int y, int size, Color color) {
+        g2.setColor(color);
+        int r = size/2;
+        int n = 5;
+        int[] xs = new int[n*2], ys = new int[n*2];
+        for (int i = 0; i < n*2; i++) {
+            double angle = Math.PI/2 + i * Math.PI/n;
+            int rad = (i%2==0) ? r : r/2;
+            xs[i] = x + (int)(Math.cos(angle)*rad);
+            ys[i] = y - (int)(Math.sin(angle)*rad);
+        }
+        g2.fillPolygon(xs, ys, n*2);
+    }
+    private void drawFlower(Graphics2D g2, int x, int y, int size, Color color) {
+        g2.setColor(color);
+        int petals = 6;
+        int r = size/2;
+        for (int i = 0; i < petals; i++) {
+            double angle = 2*Math.PI*i/petals;
+            int px = x + (int)(Math.cos(angle)*r*0.7);
+            int py = y + (int)(Math.sin(angle)*r*0.7);
+            g2.fillOval(px - r/3, py - r/3, 2*r/3, 2*r/3);
+        }
+        g2.setColor(Color.YELLOW);
+        g2.fillOval(x - r/3, y - r/3, 2*r/3, 2*r/3);
+    }
+
+    private void sprayAt(int x, int y) {
+        Graphics2D g2 = canvasImage.createGraphics();
+        g2.setColor(currentColor);
+        int radius = toolSize / 2;
+        int dots = Math.max(10, toolSize * 4); // density
+        for (int i = 0; i < dots; i++) {
+            double angle = Math.random() * 2 * Math.PI;
+            double dist = Math.random() * radius;
+            int dx = (int) (Math.cos(angle) * dist);
+            int dy = (int) (Math.sin(angle) * dist);
+            g2.fillRect(x + dx, y + dy, 1, 1);
+        }
+        g2.dispose();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(canvasImage, 0, 0, null);
+        // Draw cursor preview if not drawing and cursor is in panel
+        if (!drawing && cursorInPanel && cursorX >= 0 && cursorY >= 0) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            float alpha = 0.5f;
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+            switch (currentTool) {
+                case PENCIL:
+                case BRUSH:
+                    g2.setColor(currentTool == Tool.ERASER ? Color.WHITE : currentColor);
+                    g2.fillOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    g2.setColor(Color.BLACK);
+                    g2.setStroke(new BasicStroke(1.5f));
+                    g2.drawOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    break;
+                case SPRAYCAN:
+                    g2.setColor(currentColor);
+                    g2.drawOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    for (int i = 0; i < Math.max(10, toolSize * 4); i++) {
+                        double angle = Math.random() * 2 * Math.PI;
+                        double dist = Math.random() * (toolSize / 2);
+                        int dx = (int) (Math.cos(angle) * dist);
+                        int dy = (int) (Math.sin(angle) * dist);
+                        g2.fillRect(cursorX + dx, cursorY + dy, 1, 1);
+                    }
+                    break;
+                case ERASER:
+                    g2.setColor(Color.WHITE);
+                    g2.fillOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    g2.setColor(Color.GRAY);
+                    g2.setStroke(new BasicStroke(1.5f));
+                    g2.drawOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    break;
+                case FILL:
+                    g2.setColor(currentColor);
+                    g2.fillOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    g2.setColor(Color.BLACK);
+                    g2.setStroke(new BasicStroke(1.5f));
+                    g2.drawOval(cursorX - toolSize/2, cursorY - toolSize/2, toolSize, toolSize);
+                    break;
+                case STAMP:
+                    drawStampPreview(g2, cursorX, cursorY, toolSize, currentColor, currentStamp);
+                    break;
+            }
+            g2.dispose();
+        }
+    }
+
+    // Draws a semi-transparent preview of the stamp
+    private void drawStampPreview(Graphics2D g2, int x, int y, int size, Color color, StampType type) {
+        Composite oldComp = g2.getComposite();
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+        switch (type) {
+            case SMILEY:
+                drawSmiley(g2, x, y, size, color);
+                break;
+            case CAT:
+                drawCat(g2, x, y, size, color);
+                break;
+            case STAR:
+                drawStar(g2, x, y, size, color);
+                break;
+            case FLOWER:
+                drawFlower(g2, x, y, size, color);
+                break;
+        }
+        g2.setComposite(oldComp);
     }
 
     public enum Tool {
-        PENCIL, BRUSH, FILL, ERASER
+        PENCIL, BRUSH, SPRAYCAN, FILL, ERASER, STAMP
+    }
+
+    public boolean saveImage(File file) {
+        try {
+            return ImageIO.write(canvasImage, "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean openImage(File file) {
+        try {
+            BufferedImage img = ImageIO.read(file);
+            if (img == null) return false;
+            Graphics2D g2 = canvasImage.createGraphics();
+            g2.setColor(Color.WHITE);
+            g2.fillRect(0, 0, canvasImage.getWidth(), canvasImage.getHeight());
+            // Scale image to fit if needed
+            int w = canvasImage.getWidth();
+            int h = canvasImage.getHeight();
+            g2.drawImage(img, 0, 0, w, h, null);
+            g2.dispose();
+            repaint();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
